ff34f9b79009132f20e0f0d412d168af3a3eecf3
huaihong1986 <huaihong1986@sina.com>
2015-08-26 13:02:05 +0800

    Just add a class

--- GitDemo/ViewController.h
+++ GitDemo/ViewController.h
@@ -5,11 +5,13 @@
 //  Created by Hu Aihong on 15-8-26.
 //  Copyright (c) 2015年 ChinaCloud. All rights reserved.
 //
-
+#import "TestClass.h"
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
 
 @property (nonatomic) int sum;
+
+@property (nonatomic, strong) TestClass *testClass;
 @end
 
